6a86b92f234421c2f337a4ecede9ecdf8d53db62
elev <elev@192.168.1.105>
2017-03-17 16:00:25 +0100

    uppdaterad bookSeat()

--- Cinema/src/cinema/Viewing.java
+++ Cinema/src/cinema/Viewing.java
@@ -11,12 +11,16 @@ public class Viewing implements Comparable<Viewing> {
 	private LocalDateTime startTime;
 	private LocalDateTime endTime;
 	private String[] seats;
-	
+
 	/**
 	 * The end time of the film will be calculated in this constructor.
-	 * @param film - A reference to a film-object.
-	 * @param startTime - Start time, and date, of the film.
-	 * @param seatAmount - Amount of seats in the theater.
+	 * 
+	 * @param film
+	 *            - A reference to a film-object.
+	 * @param startTime
+	 *            - Start time, and date, of the film.
+	 * @param seatAmount
+	 *            - Amount of seats in the theater.
 	 */
 	public Viewing(Film film, LocalDateTime startTime, int seatAmount) {
 		this.setFilm(film);
@@ -24,7 +28,7 @@ public class Viewing implements Comparable<Viewing> {
 		setEndTime(startTime.plusMinutes(film.getLength().toMinutes()));
 		seats = new String[seatAmount];
 	}
-	
+
 	/**
 	 * Overloaded constructor adapted for loading Strings from file. ish
 	 */
@@ -33,17 +37,23 @@ public class Viewing implements Comparable<Viewing> {
 		this.setStartTime(startTime);
 		setEndTime(startTime.plusMinutes(film.getLength().toMinutes()));
 		this.seats = new String[seats.length];
-		
+
 		for (int i = 0; i < seats.length; i++) {
 			this.seats[i] = seats[i];
 		}
 	}
-	
+
 	public boolean bookSeat(int seat, String ticketID) {
-		seats[seat] = ticketID;
-		return true;
+		boolean success;
+		if (seats[seat] != null) {
+			seats[seat] = ticketID;
+			success = true;
+		} else {
+			success = false;
+		}
+		return success;
 	}
-	
+
 	public boolean bookSeats(int seat, String ticketID, int seatCount) {
 		return false;
 	}
@@ -71,12 +81,12 @@ public class Viewing implements Comparable<Viewing> {
 	void setEndTime(LocalDateTime endTime) {
 		this.endTime = endTime;
 	}
-	
+
 	/**
-	 * @return 0 if o  
+	 * @return 0 if o
 	 */
 	@Override
-	public int compareTo(Viewing o) {		
+	public int compareTo(Viewing o) {
 		int result;
 		if (getStartTime().isBefore(o.getStartTime()) && getEndTime().isBefore(o.getStartTime()))
 			result = -1;
@@ -87,15 +97,15 @@ public class Viewing implements Comparable<Viewing> {
 		}
 		return result;
 	}
-	
-	
+
 	@Override
 	public String toString() {
 		return film.getTitle() + ", " + startTime.toString() + ", " + endTime.toString();
 	}
-	
+
 	/**
-	 * @return All data from Viewing, including all seats, and their respective ticketID's.
+	 * @return All data from Viewing, including all seats, and their respective
+	 *         ticketID's.
 	 */
 	public String toFileString() {
 		String fileString = "Viewing:" + film.getTitle() + "," + startTime.toString() + "Seats:";
