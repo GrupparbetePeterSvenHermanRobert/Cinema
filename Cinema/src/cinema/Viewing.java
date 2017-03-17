958fe2562f454fe3eef1869082764ee5c5526307
elev <elev@192.168.1.105>
2017-03-17 13:25:40 +0100

    Override toString i syfte att spara information om Viewing

--- Cinema/src/cinema/Viewing.java
+++ Cinema/src/cinema/Viewing.java
@@ -41,8 +41,7 @@ public class Viewing implements Comparable<Viewing> {
 
 	@Override
 	public int compareTo(Viewing o) {
-		
-		return 0;
+		return getStartTime().compareTo(o.getStartTime());
 	}
 	
 	
