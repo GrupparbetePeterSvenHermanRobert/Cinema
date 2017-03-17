fce1a5c9ece39738726b0c77114f43908ec6cee3
Draelgar <g.peter.hultgren@gmail.com>
2017-03-17 14:01:24 +0100

    Shell Theater

--- Cinema/src/cinema/Theater.java
+++ Cinema/src/cinema/Theater.java
@@ -1,5 +1,6 @@
 package cinema;
 
+import java.time.LocalDateTime;
 import java.util.SortedSet;
 
 /** This class represents a single theater within a cinema.
@@ -7,7 +8,7 @@ import java.util.SortedSet;
  * @version 0.2 **/
 public class Theater {
 	/** The identification number of this theater. **/
-	private int theatreId;
+	private int theaterId;
 	/** The amount of seat rows within this theater. **/
 	private int seatRows;
 	/** The amounts of seat columns within this theater. **/
@@ -15,6 +16,53 @@ public class Theater {
 	/** A set of viewings sorted by time. **/
 	private SortedSet<Viewing> viewingList;
 	
+	/** Create a new theater.
+	 * @param theaterId -The ID number of this theater.
+	 * @param seatRows -The amount of seat rows.
+	 * @param seatColumns -The amount of seat columns. **/
+	public Theater(int theaterId, int seatRows, int seatColumns) {
+		this.seatRows = seatRows;
+		this.seatColumns = seatColumns;
+		this.theaterId = theaterId;
+	}
 	
+	/** Create a new theater.
+	 * @param data -An array of the data required to create a new theater. **/
+	public Theater(String[] data) {
+		// TODO process the data and create the object.
+	}
 	
+	/** Get the theater ID.
+	 * @return The identification number for this theater. **/
+	public int getId() {
+		return theaterId;
+	}
+	
+	/** Get the amount of seat rows available.
+	 * @return The amount of seat rows.**/
+	public int getSeatRows() {
+		return seatRows;
+	}
+	
+	/** Get the amount of seat columns available.
+	 * @return The amount of seat columns.**/
+	public int getSeatColumns() {
+		return seatColumns;
+	}
+	
+	public boolean addViewing(Film film, LocalDateTime start) {
+		return true;
+	}
+	
+	public Viewing getViewing(Film film, LocalDateTime start) {
+		return null;
+	}
+	
+	public String[] getViewings() {
+		return null;
+	}
+	
+	public String[] dataToString() {
+		return null;
+	}
 }
