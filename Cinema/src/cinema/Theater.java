935f57729f6d47126642f429e24f28e232a10693
Draelgar <g.peter.hultgren@gmail.com>
2017-03-17 13:45:42 +0100

    Spell check

--- Cinema/src/cinema/Theater.java
+++ Cinema/src/cinema/Theater.java
@@ -2,15 +2,15 @@ package cinema;
 
 import java.util.SortedSet;
 
-/** This class represents a single theatre within a cinema.
+/** This class represents a single theater within a cinema.
  * @author Gustaf Peter Hultgren
  * @version 0.2 **/
-public class Theatre {
-	/** The identification number of this theatre. **/
+public class Theater {
+	/** The identification number of this theater. **/
 	private int theatreId;
-	/** The amount of seat rows within this theatre. **/
+	/** The amount of seat rows within this theater. **/
 	private int seatRows;
-	/**  **/
+	/** The amounts of seat columns within this theater. **/
 	private int seatColumns;
 	/** A set of viewings sorted by time. **/
 	private SortedSet<Viewing> viewingList;
