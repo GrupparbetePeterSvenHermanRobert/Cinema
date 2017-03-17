3fef6d1b5911c9dd64f2ac26c8ea66d1b008fa6e
elev <elev@192.168.1.112>
2017-03-17 10:11:42 +0100

    Sven har gjort en biljett

--- Cinema/src/cinema/Ticket.java
+++ Cinema/src/cinema/Ticket.java
@@ -1,5 +1,78 @@
 package cinema;
-
+/**this is the Ticket
+ * @author Sven
+ * @version 0.1
+ */
 public class Ticket {
+
+	
+/** Ticket ID**/
+private String ticketId;
+
+/** viewing **/
+private Viewing viewing;
+
+/**the seat number in the theatre**/
+private int seat;
+
+
+public Viewing getViewing() {
+	return viewing;
+}
+
+
+public void setViewing(Viewing viewing) {
+	this.viewing = viewing;
+}
+
+
+public int getSeat() {
+	return seat;
+}
+
+
+public void setSeat(int seat) {
+	this.seat = seat;
+}
+
+
+public int getTheatreId() {
+	return theatreId;
+}
+
+
+public void setTheatreId(int theatreId) {
+	this.theatreId = theatreId;
+}
+
+
+public String getTicketId() {
+	return ticketId;
+}
+
+
+int theatreId;
+
+
+/**the id of the Theatre
+ * @param ticketId
+ * @param viewing
+ * @param seat
+ * @param theatreId
+ **/
+public Ticket( String ticketId,Viewing viewing, int seat, int theatreId ){
+	this.ticketId= ticketId;
+	this.viewing = viewing;
+	this.seat = seat;
+	this.theatreId= theatreId;
+}
+	
+
+
+
+
+
+
+
 
 }
