44dff46f4edf2ccc9fb2469bde55b83ba5fc781e
Draelgar <g.peter.hultgren@gmail.com>
2017-03-23 15:21:04 +0100

    Added some dummies for the database.

---
+++ Cinema/dummyData.sql
@@ -0,0 +1,76 @@
+USE cinema;
+
+INSERT INTO film
+VALUES ('Lord of the Rings: Fellowship of the Ring', 145, 'A good fantasy film.');
+
+INSERT INTO film
+VALUES ('Lord of the Rings: The Two Towers', 145, 'A good fantasy film.');
+
+INSERT INTO film
+VALUES ('Lord of the Rings: Return of the King', 145, 'A good fantasy film.');
+
+INSERT INTO film
+VALUES ('Matrix', 125, 'A good sci-fi film.');
+
+INSERT INTO film
+VALUES ('Terminator', 125, 'A sci.fi film.');
+
+INSERT INTO film
+VALUES ('Star Wars: The Clone Wars', 125, 'A good sci-fi film.');
+
+INSERT INTO theater
+VALUES(0, 4, 5);
+
+INSERT INTO theater
+VALUES(1, 10, 7);
+
+INSERT INTO theater
+VALUES(2, 8, 15);
+
+INSERT INTO viewing
+VALUES(0, 'Matrix', "2017-05-30 19:30:00", "2017-05-30 21:35:00", 0);
+
+INSERT INTO bookedseat
+VALUES(0, NULL, 0);
+INSERT INTO bookedseat
+VALUES(1, NULL, 0);
+INSERT INTO bookedseat
+VALUES(2, NULL, 0);
+INSERT INTO bookedseat
+VALUES(3, NULL, 0);
+
+INSERT INTO bookedseat
+VALUES(4, NULL, 0);
+INSERT INTO bookedseat
+VALUES(5, NULL, 0);
+INSERT INTO bookedseat
+VALUES(6, NULL, 0);
+INSERT INTO bookedseat
+VALUES(7, NULL, 0);
+
+INSERT INTO bookedseat
+VALUES(8, NULL, 0);
+INSERT INTO bookedseat
+VALUES(9, NULL, 0);
+INSERT INTO bookedseat
+VALUES(10, NULL, 0);
+INSERT INTO bookedseat
+VALUES(11, NULL, 0);
+
+INSERT INTO bookedseat
+VALUES(12, NULL, 0);
+INSERT INTO bookedseat
+VALUES(13, NULL, 0);
+INSERT INTO bookedseat
+VALUES(14, NULL, 0);
+INSERT INTO bookedseat
+VALUES(15, NULL, 0);
+
+INSERT INTO bookedseat
+VALUES(16, NULL, 0);
+INSERT INTO bookedseat
+VALUES(17, NULL, 0);
+INSERT INTO bookedseat
+VALUES(18, NULL, 0);
+INSERT INTO bookedseat
+VALUES(19, NULL, 0);
