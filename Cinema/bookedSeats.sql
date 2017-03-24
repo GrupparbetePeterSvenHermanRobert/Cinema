e9a2bb0d2569ff913a835f67d377e65027586916
Robert Fängström <Robert-Fang@github.com>
2017-03-24 09:08:26 +0100

    Lade in bookedSeats table

---
+++ Cinema/bookedSeats.sql
@@ -0,0 +1,57 @@
+#Bookedseats (fill_list, get_seat_by_id, clear_by_viewing)
+USE cinema;
+DELIMITER $$
+USE `cinema`$$
+create procedure `add_seat` (
+IN id SMALLINT(5) UNSIGNED, 
+in tickedID VARCHAR(50),
+IN viewingID SMALLINT(5) UNSIGNED)
+BEGIN
+INSERT INTO bookedseat
+VALUES(id, ticketid, viewingID);
+
+ 
+
+END $$
+
+DELIMITER ;
+
+
+DELIMITER $$
+USE `cinema`$$
+CREATE PROCEDURE `get_seat_by_id`(
+in id smallint  (5) UNSIGNED)
+
+BEGIN
+SELECT * FROM bookedseat
+WHERE bookedseat.id = id;
+
+END $$
+DELIMITER ;
+
+DELIMITER $$
+USE `cinema`$$
+create procedure `clear_by_viewing`(
+
+IN viewingID SMALLINT (5) UNSIGNED
+
+)
+BEGIN
+DELETE FROM bookedseat 
+WHERE bookedseat.viewingID = viewingID;
+END $$
+
+DELIMITER ;
+
+
+USE cinema;
+DELIMITER $$
+USE `cinema`$$
+CREATE PROCEDURE `get_seat_by_viewing` (
+IN viewingID SMALLINT (5) UNSIGNED)
+BEGIN
+SELECT * FROM bookedseat
+WHERE bookedseat.viewingID = viewingID;
+END$$
+DELIMITER ;
+
