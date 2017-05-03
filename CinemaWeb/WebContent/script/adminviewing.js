0d3825128b3eae16978df72d02c63ac13f23560b
drael <drael@Draelgar>
2017-05-03 15:32:11 +0200

    Added comments and commented out the film list as it is never used.

--- CinemaWeb/WebContent/script/adminviewing.js
+++ CinemaWeb/WebContent/script/adminviewing.js
@@ -6,9 +6,11 @@
 	
 	var app = angular.module("avApp", []);
 	
-	var theaterController = function($scope, $http) {		
+	var theaterController = function($scope, $http) {
+		
+		// This function triggers whenever a call to get the data from the database is successful.
 		var onComplete = function(response) {
-			$scope.films = response.data.films;
+			//$scope.films = response.data.films;
 			$scope.viewings = response.data.viewings;
 			$scope.theaters = response.data.theaters;
 			
@@ -17,35 +19,46 @@
 			if($scope.viewings.length > 0) {
 				$scope.selectedViewing = $scope.viewings[0];
 			}
+			
+			$scope.error = "";
 		}
 		
+		// Function to trigger whenever an error has occured.
 		var onError = function(reason) {
 			$scope.error = "Error: " + reason.data.status;
+			alert($scope.error);
 		}
 		
 		// Call the servlet for data.
 		$http.get("GetViewingsAdmin").then(onComplete, onError);
 		
+		// Triggers whenever the filter is changed.
 		$scope.filter = function() {
 			var id = $scope.selectedTheater;
 			
+			// If id = -1, then no filter is used and all viewings shown.
 			if(id == -1)
 				$scope.viewings = $scope.viewingsList;
+			
+			// Else, only viewings associated with the selected theater should be shown.
 			else
 				$scope.viewings = $scope.viewingsList.filter(function(viewing){
 					return viewing.theaterId == id;
 				})
 		}
 		
+		// Triggers when a viewing is selected.
 		$scope.selectViewing = function(viewing) {
 			$scope.selectedViewing = viewing;
 		}
 		
+		// Triggers if a http request to add a new viewing was successful.
 		var onAddComplete = function(response) {
 			// Call the servlet for data.
 			$http.get("GetViewingsAdmin").then(onComplete, onError);
 		}
 		
+		// Triggers whenever a http request to remove a viewing was successful.
 		var onRemoveComplete = function(response) {
 			// Remove the old data from the list.
 			$scope.viewingsList = $scope.viewingsList.filter(function(){
@@ -54,6 +67,7 @@
 			
 		}
 		
+		// Triggers when adding a new viewing.
 		$scope.add = function() {
 			var package = {
 					mode: "add",
@@ -70,6 +84,7 @@
 			$http.post("GetViewingsAdmin", jsonPackage).then(onAddComplete, onError);
 		}
 		
+		// Triggers when removing a viewing.
 		$scope.remove = function(viewingId) {
 			var package = {
 					mode: "delete",
@@ -84,6 +99,7 @@
 		}
 	}
 	
+	// Bind the controller to the application.
 	app.controller("theaterController", ["$scope", "$http", theaterController]);
 	
 }())
